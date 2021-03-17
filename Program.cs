89408575cffda71af6c999a8b90f531dce39331e
Kaylany Paulus <kaylanysilvapaulus@gmail.com>
2021-03-17 17:01:13 -0300

    Exibição do nome da escola em que estudo.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Etec_exercicios_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etec Adolpho Berezin");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
